5763f1a4861d959650e9188a654cc7792ed51686
SaulSandovalM <sauldevelop@gmail.com>
2017-08-01 10:53:10 -0500

    cambioslogin

--- assets/Login.js
+++ assets/Login.js
@@ -1,90 +1,165 @@
 import React, {Component} from 'react';
-import {View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { Button, Icon, Item, Input, Container, Content} from 'native-base';
+import {View, Text, StyleSheet, Image, TextInput} from 'react-native';
 import {firebaseRef} from './Firebase';
-import imgLogo from '../imgs/planb.png';
-import {Actions} from 'react-native-router-flux';
+import {Button, Icon, Item, Input} from 'native-base';
+import FBSDK, {LoginButton, AccessToken} from 'react-native-fbsdk';
 
 class Login extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      email: '',
-      password: '',
-      status: ''
-    }
-    this._login = this._login.bind(this)
-    this._reg = this._reg.bind(this)
+  state = {
+    email: '',
+    contraseña: '',
+    error: '',
+    credential: ''
+  };
+
+  componentWillMount() {
+    this.authenticateUser();
   }
 
-  _login() {
-    firebaseRef.auth().signInWithEmailAndPassword(this.state.email, this.state.password).catch(function(error) {
-      console.log(error.code)
-      console.log(error.message)
+  authenticateUser = () => {
+    AccessToken.getCurrentAccessToken().then((data) => {
+      const {accessToken} = data
+      const credential = FacebookAuthProvider.credential(accessToken)
+      firebaseRef.signInWithCredential(credential).then((credentials) => {
+        this.setState({credentials})
+
+      }, (error) => {
+        console.log("Sign in error", error)
+      })
     })
-     Actions.Inicio()
   }
 
-  _reg(){
-    Actions.Registro()
+  onButtonPress() {
+    const {email, contraseña} = this.state;
+
+    this.setState({error: ''});
+
+    firebaseRef.auth().signInWithEmailAndPassword(email, contraseña).then(this.onLoginSuccess.bind(this)).catch(() => {
+      firebaseRef.auth().createUserWithEmailAndPassword(email, contraseña).then(this.onLoginSuccess.bind(this))
+      .catch(this.onLoginFailed.bind(this));
+    });
+  }
+
+  onLoginFailed() {
+    this.setState({error: 'Autenticación Fallida'});
+    alert('Verifica campos Perro')
+  }
+
+  onLoginSuccess() {
+    this.setState({email: '', contraseña: '', error: ''});
   }
 
   render() {
     return (
-      <Container style={{backgroundColor: "white"}}>
-        <Content>
-          <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-around'}}>
-            <View style={{flex: 2}}>
-              <Image source={imgLogo} style={styles.image}/>
-            </View>
+      <Image source={require('../imgs/fn.jpg')} style={styles.imagen}>
 
-            <View style={{height: 70, flexDirection: 'row', justifyContent: 'space-around'}}>
-              <TouchableOpacity style={{margin: 10}}>
-                <Icon name="logo-facebook" style={{color: '#3B5998', fontSize: 40}} />
-              </TouchableOpacity>
+        <Text style={styles.texto}>BIENVENIDO</Text>
 
-              <TouchableOpacity style={{ margin: 10 }}>
-                <Icon name="logo-googleplus" style={{ color: '#c71610', fontSize: 40 }}/>
-              </TouchableOpacity>
-            </View>
+        <View style={styles.view1}>
 
-            <Item rounded style={styles.inputRounded}>
-              <Input placeholder='Correo electrónico' keyboardType='email-address' returnKeyType='next'
-                onChangeText={(text) => this.setState({email: text})} value={this.state.email}/>
-            </Item>
-            <Item rounded style={styles.inputRounded}>
-              <Input placeholder='Contraseña' secureTextEntry={true}
-                onChangeText={(text) => this.setState({password: text})} value={this.state.password}/>
-            </Item>
-            <Button rounded block style={{marginRight: 40, marginLeft: 40, marginBottom: 10, backgroundColor: '#4DA49B'}}
-               onPress={this._login}>
-              <Text style={{ color: 'white' }}>INGRESAR</Text>
-            </Button>
+          <LoginButton
+          readPermissions={['public_profile', 'email']}
+          onLoginFinished={ this.handleLoginFinished }
+          onLogoutFinished={() => alert("Adios perro.")}/>
+        </View>
 
-            <View style={{ justifyContent: 'flex-end' }}>
-              <View style={{justifyContent: 'center', flexDirection: 'row', marginBottom: 10, marginTop: 10 }}>
-                <Text>¿Aún no tienes cuenta?</Text>
-                <Text style={{ fontWeight: 'bold' }} onPress={this._reg}>REGISTRATE</Text>
-              </View>
-            </View>
+        <Item rounded style={styles.inputRounded}>
+          <Input style={styles.input}
+          placeholder='Correo electrónico' keyboardType='email-address' placeholderTextColor='white'
+          returnKeyType='next' value={this.state.text} onChangeText={email => this.setState({email})}/>
+        </Item>
+
+        <Item rounded style={styles.inputRounded}>
+          <Input style={styles.input}
+          placeholder='Contraseña' placeholderTextColor='white' secureTextEntry={true}
+          value={this.state.contraseña} onChangeText={contraseña => this.setState({contraseña})}/>
+        </Item>
+
+        <Button rounded block style={styles.buttonIngreso} onPress={this.onButtonPress.bind(this)}>
+          <Text style={styles.boton}>INGRESAR</Text>
+        </Button>
+
+        <View style={styles.view2}>
+          <View style={styles.view3}>
+            <Text>¿Aún no tienes cuenta?,</Text>
+            <Text style={styles.registrate}>REGISTRATE</Text>
           </View>
-        </Content>
-      </Container>
+        </View>
+      </Image>
     );
+  }
+}
+
+handleLoginFinished = (error, result) => {
+  if (error) {
+    console.error(error)
+  } else if (result.isCancelled) {
+    console.warn("login is cancelled.");
+  } else {
+    this.authenticateUser()
+    alert('FuncionHandle')
   }
 }
 
 const styles = StyleSheet.create({
   image: {
     width: '100%',
-    height: 300
+    height: '100%'
   },
   inputRounded: {
     marginRight: 40,
     marginLeft: 40,
     marginBottom: 10,
-    borderColor: '#4DA49B'
+    borderColor: '#f08080',
+    borderWidth: 3
   },
-});
+  buttonIngreso: {
+    marginRight: 40,
+    marginLeft: 40,
+    marginBottom: 10,
+    backgroundColor: '#f08080'
+  },
+  imagen: {
+    justifyContent: 'space-around',
+    flex: 2,
+    height: null,
+    width: null,
+    opacity: 15
+  },
+  texto: {
+    color: 'white',
+    fontSize: 40,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    marginTop: 20,
+    backgroundColor: 'transparent'
+  },
+  view1: {
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  view2: {
+    justifyContent: 'flex-end',
+    backgroundColor: 'transparent'
+  },
+  view3: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    marginBottom: 10,
+    marginTop: 10
+  },
+  input: {
+    color: 'white'
+  },
+  boton: {
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  registrate: {
+    fontWeight: 'bold'
+  }
+})
 
 export default Login;
